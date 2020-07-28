d54957cccf1760af1f0cd51de59589ac08be658f
Andres Naranjo <manazutec@gmail.com>
2020-07-28 09:31:24 -0600

    Creating getArticle

--- extract.R
+++ extract.R
@@ -77,7 +77,64 @@ str(res_tabla)
 
 resultado_aspiradoras<-c(nombre_texto, precio_texto, opiniones_texto, as.character(res_tabla$`Peso del producto`), as.character(res_tabla$Potencia), as.character(res_tabla$`Dimensiones del producto`), as.character(res_tabla$Volumen))
 
-
+getArticulo<-function(url){
+  pagina_web<-read_html(url)
+  nombre_nodo<-html_node(pagina_web, nombre)
+  nombre_texto<-html_text(nombre_nodo)
+  nombre_texto
+  
+  opiniones<-"#acrCustomerReviewText"
+  opiniones_nodo<-html_node(pagina_web, opiniones)
+  opiniones_texto<-html_text(opiniones_nodo)
+  opiniones_texto
+  
+  precio<-"#priceblock_ourprice"
+  precio_nodo<-html_node(pagina_web, precio)
+  precio_texto<-html_text(precio_nodo)
+  precio_texto
+  
+  tabla<-"#prodDetails > div.wrapper.ESlocale > div.column.col1 > div > div.content.pdClearfix > div > div > table"
+  tabla_nodo<-html_node(pagina_web, tabla)
+  if(!is.na(tabla_nodo)){ #SI el nodo no esta vacio!!!
+    tabla_tab<-html_table(tabla_nodo)
+    tabla_tab
+    val<-tabla_tab$X2
+    res_tabla<-data.frame(t(val))
+    tabla_name<-tabla_tab$X1
+    colnames(res_tabla)<-tabla_name
+  }
+  col<-c("Peso del producto", "Dimensiones del producto", "Volumen", "Potencia")
+  if( is.na(tabla_nodo)){ #Si tabla nodo esta vacia
+    #Rellenar con campos vacio
+    mitab<-data.frame(colnames(col))
+    mitab<-rbind(mitab, c("-1", "-1", "-1","-1"))
+    colnames(mitab)<-col
+  }else{
+    #Evaluar cada uno de los campos
+    zero<-matrix("-1", ncol=4, nrow=1)
+    dfzero<-as.data.frame(zero)
+    colnames(dfzero)<-col
+    dfzero
+    peso<-as.character(res_tabla$`Peso del producto`)
+    if(length(peso)==0) peso <- "-1"
+    volumen<-as.character(res_tabla$Volumen)
+    if(length(volumen)==0) volumen <- "-1"
+    dime<-as.character(res_tabla$`Dimensiones del producto`)
+    if(length(dime)==0) dime <- "-1"
+    potencia<-as.character(res_tabla$Potencia)
+    if(length(potencia)==0) potencia <- "-1"
+    dfzero$`Peso del producto`<-peso
+    dfzero$`Dimensiones del producto`<-dime
+    dfzero$Volumen<-volumen
+    dfzero$Potencia<-potencia
+    str(dfzero)
+    mitab<-dfzero
+    colnames(mitab)<-col
+  }
+  articulo<-c(nombre_texto, as.character(mitab$`Peso del producto`[1]), as.character(mitab$`Dimensiones del producto`[1]), as.character(mitab$Volumen[1]),as.character(mitab$Potencia[1]), opiniones_texto, precio_texto)  
+  articulo
+  
+}
 
 
 
