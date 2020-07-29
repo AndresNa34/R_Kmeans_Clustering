5c616d0c2d26226005dae80e2d0829a25f355c7c
Andres Naranjo <manazutec@gmail.com>
2020-07-29 09:26:04 -0600

    running again the scrapper

--- processing.R
+++ processing.R
@@ -4,3 +4,17 @@ res<-read.csv(file= "res_limpio.csv")
 res <- res[, -1]
 aspiradoras<-as.data.frame(res)
 View(aspiradoras)
+
+#Procesado de peso
+
+aspiradoras$`Peso del producto`<-as.character(aspiradoras$`Peso del producto`)
+aspiradoras$`Peso del producto`<-gsub(" Kg", "", aspiradoras$`Peso del producto`)
+aspiradoras$`Peso del producto`<-gsub(",", ".", aspiradoras$`Peso del producto`)
+aspiradoras$`Peso del producto`<-gsub("-1",NA, aspiradoras$`Peso del producto`)
+aspiradoras$`Peso del producto`<-as.numeric(aspiradoras$`Peso del producto`)
+pesomedio<-mean(aspiradoras$`Peso del producto`, na.rm=TRUE)
+pesomedio
+aspiradoras$`Peso del producto`[is.na(aspiradoras$`Peso del producto`)]<-pesomedio
+
+hist(aspiradoras$`Peso del producto`)
+summary(aspiradoras$`Peso del producto`)
