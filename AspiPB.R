b2e51a6feb84d187da17515062fdc5c1ab67b98e
Andres Naranjo <manazutec@gmail.com>
2020-08-01 11:04:59 -0600

    Creating Param

---
+++ AspiPB.R
@@ -0,0 +1,11 @@
+#* @param precio
+#* @param opiniones
+#* @param peso
+#* @param volumen
+#* @param potencia
+#* @param alto
+#* @param ancho
+#* @param profunidad
+#* @get /getCluster
+
+library(stringr)
